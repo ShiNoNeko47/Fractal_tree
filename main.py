943426c54289f856631bc81e775b1343a13fd235
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2021-04-19 21:17:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,22 @@
 
 import pygame
 import math
+import sys
 
-def draw_tree(window, color, n, length, length_diff, angle, angle_diff, root):
+def draw_tree(window, color, n, nmax, length, length_diff, angle, angle_diff, root):
+    recursion = True
     if n == 0:
+        pygame.display.update()
         return 0
     end = (root[0] + math.sin(angle) * length, root[1] + math.cos(angle) * length)
-    pygame.draw.line(window, color, root, end, int(n / 4))
-    draw_tree(window, color, n-1, length * length_diff, length_diff, angle + angle_diff, angle_diff, end)
-    draw_tree(window, color, n-1, length * length_diff, length_diff, angle - angle_diff, angle_diff, end)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    pygame.draw.line(window, (color), root, end, int(n / 4 + 1))
+    if recursion:
+        draw_tree(window, color, n-1, nmax, length * length_diff, length_diff, angle + angle_diff, angle_diff, end)
+    if recursion:
+        draw_tree(window, color, n-1, nmax, length * length_diff, length_diff, angle - angle_diff, angle_diff, end)
 
 def main():
     pygame.init()
@@ -18,15 +26,13 @@ def main():
     color = (255, 0, 0)
 
     loop = True
-    angle_diff = math.pi / 4
+    angle_diff = math.pi / 6
+    window.fill((0, 0, 0))
     while loop:
-        window.fill((0, 0, 0))
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                loop = False
-        angle_diff -= math.pi / 512
-        draw_tree(window, color, 13, 200, .67, -math.pi, angle_diff, (400, 600))
+        #angle_diff -= math.pi / 512
+        draw_tree(window, list(color), 15, 17, 100, .8, -math.pi, angle_diff, (400, 600))
         pygame.display.update()
 
 if __name__ == '__main__':
     main()
+
